ba5d9e33bc85473a0c0a395b7bddce11c60c1bdf
Pawel Rozlach <prozlach@gitlab.com>
2024-10-16 08:49:09 +0000

    test: rewrite in-memory tests to use testify.suite

--- registry/storage/driver/inmemory/driver_test.go
+++ registry/storage/driver/inmemory/driver_test.go
@@ -1,19 +1,42 @@
 package inmemory
 
 import (
+	"context"
 	"testing"
 
 	storagedriver "github.com/docker/distribution/registry/storage/driver"
 	"github.com/docker/distribution/registry/storage/driver/testsuites"
-	"gopkg.in/check.v1"
+	"github.com/stretchr/testify/suite"
 )
 
-// Hook up gocheck into the "go test" runner.
-func Test(t *testing.T) { check.TestingT(t) }
+func TestInMemoryDriverSuite(t *testing.T) {
+	ts := testsuites.NewDriverSuite(
+		context.Background(),
+		func() (storagedriver.StorageDriver, error) {
+			return New(), nil
+		},
+		nil,
+	)
+	suite.Run(t, ts)
+}
 
-func init() {
-	inmemoryDriverConstructor := func() (storagedriver.StorageDriver, error) {
-		return New(), nil
+func BenchmarkInMemoryDriverSuite(b *testing.B) {
+	ts := testsuites.NewDriverSuite(
+		context.Background(),
+		func() (storagedriver.StorageDriver, error) {
+			return New(), nil
+		},
+		nil,
+	)
+
+	ts.SetupSuiteWithB(b)
+	b.Cleanup(func() { ts.TearDownSuiteWithB(b) })
+
+	// NOTE(prozlach): This is a method of embedded function, we need to pass
+	// the reference to "outer" struct directly
+	benchmarks := ts.EnumerateBenchmarks()
+
+	for _, benchmark := range benchmarks {
+		b.Run(benchmark.Name, benchmark.Func)
 	}
-	testsuites.RegisterSuite(inmemoryDriverConstructor, testsuites.NeverSkip)
 }
