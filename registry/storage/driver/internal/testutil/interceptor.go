5989be50efc6d2e16ddd5dbd4eef53103575644c
Pawel Rozlach <prozlach@gitlab.com>
2025-02-17 23:35:43 +0000

    test: small improvements to the interceptor helper used in azure tests

--- registry/storage/driver/internal/testutil/interceptor.go
+++ registry/storage/driver/internal/testutil/interceptor.go
@@ -3,14 +3,19 @@ package testutil
 import (
 	"bytes"
 	"context"
+	"crypto/tls"
 	"fmt"
 	"io"
 	"math/rand/v2"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
 	"sync"
 	"testing"
+	"time"
+
+	"golang.org/x/net/http2"
 )
 
 const EnvMITMProxyURL = "ENV_MITM_PROXY_URL"
@@ -48,7 +53,33 @@ type InterceptorConfig struct {
 
 // NewInterceptor creates a new Interceptor with the given RoundTripper
 func NewInterceptor() (*Interceptor, error) {
-	transport := http.DefaultTransport
+	// Create transport in a way azure does it:
+	// https://github.com/Azure/azure-sdk-for-go/blob/c12b01f821a8474239e49d571d7215cebb7c0510/sdk/azcore/runtime/transport_default_http_client.go#L21-L44
+	dialer := &net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}
+	transport := &http.Transport{
+		Proxy:                 http.ProxyFromEnvironment,
+		DialContext:           dialer.DialContext,
+		ForceAttemptHTTP2:     true,
+		MaxIdleConns:          100,
+		MaxIdleConnsPerHost:   10,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		TLSClientConfig: &tls.Config{
+			MinVersion:    tls.VersionTLS12,
+			Renegotiation: tls.RenegotiateFreelyAsClient,
+		},
+	}
+	// TODO: evaluate removing this once https://github.com/golang/go/issues/59690 has been fixed
+	if http2Transport, err := http2.ConfigureTransports(transport); err == nil {
+		// if the connection has been idle for 10 seconds, send a ping frame for a health check
+		http2Transport.ReadIdleTimeout = 10 * time.Second
+		// if there's no response to the ping within the timeout, the connection will be closed
+		http2Transport.PingTimeout = 5 * time.Second
+	}
 
 	if v := os.Getenv(EnvMITMProxyURL); v != "" {
 		proxyURL, err := url.Parse(v)
