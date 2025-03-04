b00ca260c12a8925151341dadb883ba427f66601
Pawel Rozlach <prozlach@gitlab.com>
2025-03-04 10:42:32 +0100

    refactor: add s3_v2 driver

--- registry/storage/driver/s3-aws/s3.go
+++ registry/storage/driver/s3-aws/s3.go
@@ -3,9 +3,11 @@ package s3
 import (
 	"github.com/docker/distribution/registry/storage/driver/factory"
 	v1 "github.com/docker/distribution/registry/storage/driver/s3-aws/v1"
+	v2 "github.com/docker/distribution/registry/storage/driver/s3-aws/v2"
 )
 
 func init() {
 	factory.Register(v1.DriverName, new(v1.S3DriverFactory))
 	factory.Register(v1.DriverNameAlt, new(v1.S3DriverFactory))
+	factory.Register(v2.DriverName, new(v2.S3DriverFactory))
 }
