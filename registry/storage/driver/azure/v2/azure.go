0aa13fff5221800ffea73314f12e851802b67c23
Pawel Rozlach <prozlach@gitlab.com>
2025-08-11 12:04:16 +0200

    docs: add links to godocs

--- registry/storage/driver/azure/v2/azure.go
+++ registry/storage/driver/azure/v2/azure.go
@@ -122,8 +122,11 @@ func (d *driver) GetContent(ctx context.Context, targetPath string) ([]byte, err
 			MaxRetryDelay: d.maxRetryDelay,
 			ShouldRetry: func(resp *http.Response, err error) bool {
 				// NOTE(prozlach): azure-sdk-go does the same thing, we only
-				// piggy-back the metric calculation. List of codes can be found
-				// in the godoc of the policy.RetryOptions object
+				// piggy-back the metric calculation:
+				// https://github.com/Azure/azure-sdk-for-go/blob/8e0e2345f57218b29d3d411646bac932d0e4358d/sdk/azcore/runtime/policy_retry.go#L179-L190
+				// List of codes can be found in the godoc of the
+				// policy.RetryOptions object:
+				// https://github.com/Azure/azure-sdk-for-go/blob/8e0e2345f57218b29d3d411646bac932d0e4358d/sdk/azcore/policy/policy.go#L114-L124
 				statusCodes := []int{
 					http.StatusRequestTimeout,      // 408
 					http.StatusTooManyRequests,     // 429
